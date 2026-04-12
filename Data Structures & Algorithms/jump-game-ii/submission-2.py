df88841bef6ce550a47a51e78aec072170646c4f
Vaishnavi Verma <vaishnaviverma1511@gmail.com>
2026-04-12 15:28:06 -0700

    Add: jump-game-ii - submission-2

---
+++ Data Structures & Algorithms/jump-game-ii/submission-2.py
@@ -0,0 +1,14 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        n = len(nums)
+        l = r = 0
+        steps = 0
+        while r < n-1:
+            cov_range = 0
+            for i in range(l, r+1):
+                cov_range = max(cov_range, i+nums[i])
+            l = r+1
+            r = cov_range
+            steps += 1
+        return steps
+        
